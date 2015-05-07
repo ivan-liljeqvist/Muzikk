04b50521bdf0d336faf1842743625f8460f9b23e
ivan-liljeqvist <ivanli@kth.se>
2015-05-07 15:32:50 +0200

    began with spotify integration

--- src/muzikk/Main.java
+++ src/muzikk/Main.java
@@ -14,6 +14,8 @@ public class Main extends Application {
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
+
+        SpotifyHelper helper=new SpotifyHelper();
     }
 
 
