b3c3d6ac8020d458535bfad4ab51d657f5c1b773
ivan-liljeqvist <ivanli@kth.se>
2015-05-10 22:20:26 +0200

    Added server documentation

--- server/cleanup.php
+++ server/cleanup.php
@@ -1,5 +1,16 @@
-<?php
 
+<?php
+	
+	/*
+		Author - Ivan Liljeqvist 
+		Version - 10-05-2015
+
+		All this PHP file does is removing user_info.txt.
+		By doing this Muzikk erases previous logged in users before logging in a new user.
+	*/
+
+		
 	unlink('user_info.txt') or die("couldnt unlink file");
+
 
 ?>
--- server/script.js
+++ server/script.js
@@ -1,10 +1,30 @@
 
-    //window.open("http://www.w3schools.com");
+ /*
+    Author - Ivan Liljeqvist 
+    Version - 2015-05-10
+
+    This script runs on the index page Muzikk sends you to when you
+    want to log in fromt the Java client.
+
+    We have a login button that starts the Spotify login window.
+    We trigger a click on that button.
+ */
+
 (function() {
 
-
+        /*
+            ID for Muzikk from Spotify
+        */
         var CLIENT_ID = 'e45112497ae04346b38d3e6e2f73af23';
+        /*
+            Send the user here when the login has succeeded-
+        */
         var REDIRECT_URI = 'http://simkoll.com/muzikk/result.php';
+
+        /*
+            Generates the URL for Spotify login window.
+            Pass in scopes - the 'permissions'
+        */
 
         function getLoginURL(scopes) {
             return 'https://accounts.spotify.com/authorize?client_id=' + CLIENT_ID +
@@ -13,26 +33,33 @@
               '&response_type=token';
         }
         
+        /*
+            Generate URL for getting the user's mail.
+        */
+
         var url = getLoginURL(['user-read-email']);
 
+
         
-        var width = 450,
-            height = 730,
-            left = (screen.width / 2) - (width / 2),
-            top = (screen.height / 2) - (height / 2);
+        /*
+            Here we create a login button that will redirect the user 
+            to Spotify login page when clicked.
+        */
     
             
         var loginButton = document.getElementById('btn-login');
-
     
         loginButton.addEventListener('click', function() {
-            /*var w = window.open(url,
-                            'Spotify',
-                            'menubar=no,location=no,resizable=no,scrollbars=no,status=no, width=' + width + ', height=' + height + ', top=' + top + ', left=' + left
-                           );*/
 
             location.replace(url);
         });
+
+        /*
+            Ideally, we dont want the user to click the button in order to get to the login screen.
+            We want it happen automatically. Therefore we trigger the click-event on the button.
+
+            However, this might not work in some browsers. Then the user has to click on the login button.
+        */
 
         $( "#btn-login" ).trigger( "click" );
 
