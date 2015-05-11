54103feaeaa870dc5dc57148a441563c04dd5f3c
filipmartinsson <martinsson.filip@gmail.com>
2015-05-11 11:53:40 +0200

    Merged spotify integration into gui

--- src/muzikk/LoginScreenController.java
+++ src/muzikk/LoginScreenController.java
@@ -1,5 +1,6 @@
 package muzikk;
 
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Scene;
@@ -13,7 +14,7 @@ import java.util.ResourceBundle;
 /**
  * Created by filip on 2015-05-11.
  */
-public class LoginScreenController implements Initializable {
+public class LoginScreenController implements Initializable, ThreadCompleteListener {
     Stage prevStage;
     @FXML
     private Button spotifyLoginButton;
@@ -27,15 +28,42 @@ public class LoginScreenController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        //spotifyLoginButton.setOnAction((event) -> goTogame()); //start the game
+        i.setOnMouseClicked((event) -> loginSpotify()); //start the game
         noLoginButton.setOnAction(event -> goToModeSelection());
-        i.setOnMouseClicked(event -> goToModeSelection());
+    }
+    @Override
+    public void notifyOfThreadComplete(final NotifyingThread thread){
+        //extract the results from the thread
+        String user_id=thread.extractParams()[0];
+        String user_email=thread.extractParams()[1];
+        MuzikkUserInfo.setUserId(user_id);
+        MuzikkUserInfo.setUserEmail(user_email);
+        //when the user has successfully logged in in the browser
+        //we want to focus the Muzikk window, but we can't do it
+        //on this secondary thread. All UI operations have to run on Main thread.
+        //therefore we call runLater and request focus for the window inside a Runnable.
+        Platform.runLater(new Runnable() {
+            @Override
+            public void run() {
+                prevStage.requestFocus();
+                goToModeSelection();
+                System.out.println(MuzikkUserInfo.getUserId());
+            }
+        });
+
     }
 
     private void goToModeSelection(){
         ModeSelectionController controller = SceneLoader.modeSelectionLoader.getController();
         controller.setPrevStage(prevStage);
         prevStage.setScene(new Scene(SceneLoader.modeSelectionPane));
+    }
+    private void loginSpotify(){
+        MuzikkAccessFetcher.initiateLogin();
+        NotifyingThread thread = MuzikkAccessFetcher.keepPingingServerUntilUserLoggedIn();
+        thread.addListener(this);
+
+
     }
 
 }
